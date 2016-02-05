8bad328a06720c18df0dbfda64fd35f3496d96f8
Sharrell Porter <SharrellPorter@example.com>
2016-02-05 16:50:45 -0500

    assignment-30

--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -8,11 +8,27 @@ require "random_data"
 end
 posts = Post.all
 
+1.times do
+  Post.find_or_create_by(
+    title: "Travel Channel",
+    body: "I'll be home again soon!"
+  )
+end
+
+posts = Post.all
+
 
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
+  )
+end
+
+1.times do
+  Comment.find_or_create_by(
+    post: posts.sample,
+    body: "Hello Again!"
   )
 end
 
