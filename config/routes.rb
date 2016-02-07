39917bebbecf39ace454bc09e0137cc03eefc853
Sharrell Porter <SharrellPorter@example.com>
2016-02-07 09:28:31 -0500

    checkpoint-31

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
+
+  get 'about' => 'welcome#about'
+
 
   root 'welcome#index'
 
-  get 'welcome/contact'
+end
 
-  get 'welcome/faq'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -63,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
