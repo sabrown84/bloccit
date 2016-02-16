2c849036bcc0e9a2525afa66641701fc05e2d98c
Sharrell Porter <SharrellPorter@example.com>
2016-02-16 16:15:55 -0500

    assignment-35

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :sponsored_posts, dependent: :destroy
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
