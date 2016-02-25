92929fd61eba37f35a0525fe6101125c633948cc
Sharrell Porter <SharrellPorter@example.com>
2016-02-25 14:23:41 -0500

    assignment-40

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,11 +4,101 @@ include SessionsHelper
 
 RSpec.describe PostsController, type: :controller do
   let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:moderator) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :moderator) }
   let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
   let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
 
-  context "guest user" do
+context "moderator doing CRUD on a post they don't own" do
+  before do
+    create_session(moderator)
+  end
 
+  describe "GET show" do
+    it "returns http success" do
+      get :show, topic_id: my_topic.id, id: my_post.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, topic_id: my_topic.id, id: my_post.id
+      expect(response).to render_template :show
+    end
+    it "assigns my_post to @post" do
+      get :show, topic_id: my_topic.id, id: my_post.id
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new, topic_id: my_topic.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #new view" do
+      get :new, topic_id: my_topic.id
+      expect(response).to render_template :new
+    end
+    it "instantiates @post" do
+      get :new, topic_id: my_topic.id
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, topic_id: my_topic.id, id: my_post.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #edit view" do
+      get :edit, topic_id: my_topic.id, id: my_post.id
+      expect(response).to render_template :edit
+    end
+    it "assigns post to be updated to @post" do
+      get :edit, topic_id: my_topic.id, id: my_post.id
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData. random_paragraph
+
+      put :update, topic_id: my_topic.id, id: my_post.id, post: {title: new_title, body: new_body}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, topic_id: my_topic.id, id: my_post.id, post: {title: new_title, body: new_body}
+      expect(response).to redirect_to [my_topic, my_post]
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the post" do
+      delete :destroy, topic_id: my_topic.id, id: my_post.id
+      count = Post.where({id: my_post.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to posts index" do
+      delete :destroy, topic_id: my_topic.id, id: my_post.id
+      expect(response).to redirect_to my_topic
+    end
+  end
+end
+
+
+  context "guest user" do
     describe "GET show" do
       it "returns http success" do
         get :show, topic_id: my_topic.id, id: my_post.id
