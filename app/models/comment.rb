98a6937f2ace7a0fe015ba0613d0fd9d922ee0f2
Sharrell Porter <SharrellPorter@example.com>
2016-02-26 12:07:37 -0500

    assignment-42

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :user
+  belongs_to :posts
+  belongs_to :users
+  belongs_to :topics
 
   validates :body, length: { minimum: 5, presence: true}
   validates :user, presence: true
