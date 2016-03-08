6a63b176f2b7a85edf9ce587a95e6c427c2a53b4
Sharrell Porter <SharrellPorter@example.com>
2016-03-08 14:45:38 -0500

    checkpoint-48

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302164033) do
+ActiveRecord::Schema.define(version: 20160308134511) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20160302164033) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
